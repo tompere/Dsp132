442965eae81aac3146b0a386934364a835121ffb
tompere <tompere@post.bgu.ac.il>
2013-05-22 10:11:37 +0300

    1. Deal with all cases of manager states (i.e. create new instance or reboot existing); 2. Local Application creates html output files.

--- dsp132-ass1_LocalApp/src/LocalApp.java
+++ dsp132-ass1_LocalApp/src/LocalApp.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +16,16 @@ import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Filter;
 import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.RebootInstancesRequest;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
-import com.amazonaws.services.ec2.model.StartInstancesRequest;
 import com.amazonaws.services.ec2.model.Tag;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.simpleworkflow.flow.annotations.Wait;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.amazonaws.services.sqs.model.DeleteMessageRequest;
@@ -45,77 +45,72 @@ public class LocalApp {
 	private static String imagesURLlist = "text.images2.txt";
 	private static String resultLocationName;
 	private static int numberArgs; 
-	
+
 	public static void main(String[] args) throws Exception {
 
 		String taskDoneQueue = "https://sqs.us-east-1.amazonaws.com/152554501442/done_task_queue";
 		String localAppQueue = "https://sqs.us-east-1.amazonaws.com/152554501442/new_task_queue";
-		
+
 		// Obtain input arguments
 		File inputImagesFile;
-		
-		try
-		{
+
+		try{
 			inputImagesFile = new File(args[0]);
 			numberArgs = Integer.parseInt(args[1]);
 		}
 
-		catch (Exception e)
-		{
+		catch (Exception e){
 			terminateAppWithMessage("Invalid input.", e);
 			return;
 		}
 
 		// Initialize EC2 service
 		init();
-		
+
 		// upload the file with the list of images to S3
 		String eTag = s3.putObject(new PutObjectRequest("testBucketcm_sdlm4er", imagesURLlist, inputImagesFile)).getETag();
-		try
-		{	
+		try{	
 			sqs.sendMessage(new SendMessageRequest(localAppQueue,imagesURLlist + "::" + numberArgs));
 			System.out.println("File uploaded, tag is: " + eTag);
 		}
-		catch (Exception e)
-		{
+		catch (Exception e){
 			terminateAppWithMessage("Input file wasn't uploadeded", e);
 			return;
 		}
 
 		// Retrieve the manager instance from EC2
-		getInstance("Manager");
-		
+		activateInstance("Manager");
+
 		// wait to receive done message from manager via SQS
 		String queueMsg = getMessageAndDelete(taskDoneQueue);
-		while (queueMsg == "")
-		{
+		while (queueMsg == ""){
 			// do nothing and sleep
 			queueMsg = getMessageAndDelete(taskDoneQueue);
 			Thread.sleep(3000);
 		}
-		
+
 		resultLocationName = queueMsg;
-				
+
 		// retrieve result object
 		S3Object object = s3.getObject(new GetObjectRequest("testBucketcm_sdlm4er", resultLocationName));
-		InputStream objectData = object.getObjectContent();
-		int b = objectData.read();
-		while ( b > -1 )
-		{
-			System.out.print((char)b);
-			b = objectData.read();
+		try{
+			InputStream objectData = object.getObjectContent();
+			PrintWriter writer = new PrintWriter("final_output.html", "UTF-8");			
+			int b = objectData.read();
+			while ( b > -1 ){
+				writer.print((char)b);
+				b = objectData.read();
+			}
+			objectData.close();
+			writer.close();
 		}
-		objectData.close();
-
-
-
+		catch (Exception e){
+			terminateAppWithMessage("problem with reading output file", e);
+			return;
+		}
 	}
 
 	private static void init() throws Exception {
-		/*
-		 * This credentials provider implementation loads your AWS credentials
-		 * from a properties file at the root of your classpath.
-		 */
 		AWSCredentialsProvider credentialsProvider = new ClasspathPropertiesFileCredentialsProvider();
 		ec2 = new AmazonEC2Client(credentialsProvider);
 		s3  = new AmazonS3Client(credentialsProvider);
@@ -123,17 +118,16 @@ public class LocalApp {
 
 	}
 
-	private static Instance CreateInstance(String tag)
-	{
+	private static Instance createInstance(String tag){
 		RunInstancesRequest runInstancesRequest = new RunInstancesRequest();
 
 		runInstancesRequest.withImageId("ami-76f0061f")
-		.withInstanceType("t1.micro")
-		.withMinCount(1)
-		.withMaxCount(1)
-		.withKeyName("ass12")
-		.withSecurityGroups("ssh")
-		.withUserData(createScript("testBucketcm_sdlm4er","manager.jar", "manager"));
+			.withInstanceType("t1.micro")
+			.withMinCount(1)
+			.withMaxCount(1)
+			.withKeyName("ass12")
+			.withSecurityGroups("ssh")
+			.withUserData(createScript("testBucketcm_sdlm4er","manager.jar", "manager"));
 
 		RunInstancesResult runInstancesResult = 
 				ec2.runInstances(runInstancesRequest);
@@ -144,12 +138,13 @@ public class LocalApp {
 		CreateTagsRequest createTagsRequest = new CreateTagsRequest();
 		createTagsRequest.withResources(manager.getInstanceId()).withTags(new Tag("Name",tag));
 		ec2.createTags(createTagsRequest);
-
+		
+		System.out.println("Created a new " + tag + ".");
+		
 		return manager;
 	}
 
-	private static String createScript(String bucket, String key, String target)
-	{
+	private static String createScript(String bucket, String key, String target){
 		String ans = "#!/bin/bash" + "\n";
 		ans += "sudo sh" + "\n";
 		// install s3curl
@@ -177,67 +172,42 @@ public class LocalApp {
 				"--id main " +
 				"-- https://s3.amazonaws.com/" + bucket + "/" + key +
 				" > /home/ec2-user/" + target + ".jar" + "\n";
-		
+
 		// create logs file and allow full access
 		ans += "touch /home/ec2-user/manager-logs.log" + "\n";
 		ans += "chmod 777 /home/ec2-user/manager-logs.log" + "\n";
-		
+
 		// execute jar
 		ans += "java -jar /home/ec2-user/" + target + ".jar " + "\n";
 		
-		// System.out.println(ans);
-
 		return new String(Base64.encodeBase64(ans.getBytes()));
 	}
 
-	private static Instance getInstance(String tagName)
-	{
+	private static void activateInstance(String tagName){
 		List<Reservation> reservations = getInstanceHelper(tagName);
 		Instance obtainedInstance = null;
 
-		// retrieve the manager instance from EC2 and return it
-		try
-		{
+		// retrieve instance from EC2 and return it
+		try{
 			obtainedInstance = reservations.get(0).getInstances().get(0);
 			int stateCode = obtainedInstance.getState().getCode();
-			// if manager is terminated or in shutting down process
+
+			// if instance is terminated or in shutting down process - create new manager
 			if (stateCode == 48 || stateCode == 32)
-			{
-				return CreateInstance(tagName);
-			}
-			
-			// TODO - in order to run jar, manager needs to start over.
-			// so, if manager is not being created, it should be rebooted
-			
-			// if manager is stopped or stopping
-			else if (stateCode == 64 || stateCode == 80)
-			{
-				startIntance(obtainedInstance.getInstanceId());
-				return obtainedInstance;
-			}
-			// if manage is pending or running
-			else 
-			{
-				// block until manager started running
-				while (stateCode != 16)
-				{
-					obtainedInstance = getInstanceHelper(tagName).get(0).getInstances().get(0);
-					stateCode = obtainedInstance.getState().getCode();
-				}
-				return obtainedInstance;	
-			}
+				createInstance(tagName);
+
+			// if instance is stopped, stopping, pending or running - reboot current manager
+			else
+				rebootIntance(obtainedInstance.getInstanceId());
 		}
 
-		// in case no manager was was return null
-		catch (Exception e)
-		{	
-			System.out.println("Creating a new manager.");
-			return CreateInstance("Manager");
+		// in case no instance was found - create new instance
+		catch (NullPointerException e){	
+			createInstance(tagName);
 		}
 	}
 
-	private static List<Reservation> getInstanceHelper(String tagName)
-	{
+	private static List<Reservation> getInstanceHelper(String tagName){
 		// create a filter
 		List<String> valuesT1 = new ArrayList<String>();
 		valuesT1.add(tagName);
@@ -248,26 +218,27 @@ public class LocalApp {
 		return result.getReservations();
 	}
 
-	private static void startIntance(String instanceID)
-	{
+	private static void rebootIntance(String instanceID){
+		System.out.println("Rebooting instance " + instanceID + ".");
 		List<String> instancesToStart = new ArrayList<String>();
 		instancesToStart.add(instanceID);
-		StartInstancesRequest starter = new StartInstancesRequest();
-		starter.setInstanceIds(instancesToStart);
-		ec2.startInstances(starter);
+		RebootInstancesRequest rebootRequest = new RebootInstancesRequest();						
+		rebootRequest.setInstanceIds(instancesToStart);
+		try {
+			ec2.rebootInstances(rebootRequest);												
+		} catch (Exception e) {	
+			terminateAppWithMessage("Problem with rebooting manager istances.", e);
+		}
 	}
-	
-	private static void terminateAppWithMessage(String msg, Exception e)
-	{
+
+	private static void terminateAppWithMessage(String msg, Exception e){
 		System.out.println(msg);
 		System.out.println("Error message: " + e.getMessage());
 		System.out.println("Program was terminated.");
 	}
-	
+
 	private static String getMessageAndDelete(String queueUrl) throws IOException{
 		String ans = "";
-		// Receive messages
-		//System.out.println("Receiving messages");
 		ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl);
 		List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
 		if (messages.size()>0){
@@ -275,10 +246,6 @@ public class LocalApp {
 			ans = message.getBody();
 			String messageRecieptHandle = message.getReceiptHandle();
 			sqs.deleteMessage(new DeleteMessageRequest(queueUrl, messageRecieptHandle));
-		}
-		else{
-			// System.out.println("queue is empty");
-			return "";
 		}
 		return ans;
 	}
